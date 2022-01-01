3e176ba820d97204855262349f09d6d913bc4e27
Charles Muojekwu <charlesmuojekwu@gmail.com>
2022-01-01 13:55:35 +0100

    error handling

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { RouterModule, Routes } from '@angular/router';
